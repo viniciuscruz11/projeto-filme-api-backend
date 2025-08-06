f370e42606a20b00eef59460cce5bbcf7cb993ff
viniciuscruz11 <viniciusfcruz@outlook.com>
2025-08-06 02:27:12 -0300

    criação backend

---
+++ projeto-filmes-api-backend/src/main/java/com/seuapp/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.seuapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.GroupedOpenApi;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**") // Configure os caminhos que você deseja documentar
+                .build();
+    }
+}
