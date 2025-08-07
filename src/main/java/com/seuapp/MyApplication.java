a4ea044057e91806d785297790877cab4b1615f9
viniciuscruz11 <viniciusfcruz@outlook.com>
2025-08-06 21:59:21 -0300

    Ajustes nas pastas, exclusão de outras desnecessárias

---
+++ src/main/java/com/seuapp/MyApplication.java
@@ -0,0 +1,18 @@
+package com.seuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableCaching
+@EnableFeignClients
+
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/seuapp/service/MovieService.java
@@ -0,0 +1,31 @@
+package main.java.com.seuapp.service;
+
+import com.seuapp.client.ExternalApiClient;
+import com.seuapp.model.MovieResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MovieService.class);
+    private final ExternalApiClient externalApiClient;
+
+    @Autowired
+    public MovieService(ExternalApiClient externalApiClient) {
+        this.externalApiClient = externalApiClient;
+    }
+
+    @Cacheable(value = "movies", key = "#title + #year + #type + #page")
+    public List<MovieResponse> searchMovies(String title, String year, String type, int page) {
+        logger.info("Searching movies with title: {}, year: {}, type: {}, page: {}", title, year, type, page);
+        List<MovieResponse> movies = externalApiClient.searchMovies(title, year, type, page);
+        logger.info("Found {} movies", movies.size());
+        return movies;
+    }
+}
